1f69bbbec6bfa73778b6f14d38f1ecd62f29b0c5
guateapps <support@wsg.com.gt>
2019-05-20 13:41:53 -0600

    Formulario de descargas de videos

--- Pro3Play/Pro3Play/Form1.Designer.cs
+++ Pro3Play/Pro3Play/Form1.Designer.cs
@@ -28,33 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.descargarVídeoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrVideo = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.ckbAudio = new System.Windows.Forms.CheckBox();
+            this.listasDeReproducciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reproductorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Product Sans Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(230, 69);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(98, 50);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(282, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Conversor de Vídeos a MP3";
             // 
-            // textBox1
+            // txtURL
             // 
-            this.textBox1.Location = new System.Drawing.Point(31, 171);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(695, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtURL.Location = new System.Drawing.Point(41, 183);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(391, 20);
+            this.txtURL.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 155);
+            this.label2.Location = new System.Drawing.Point(38, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 2;
@@ -62,25 +75,98 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(31, 197);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Product Sans", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(41, 206);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(74, 31);
             this.button1.TabIndex = 3;
             this.button1.Text = "Descargar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.descargarVídeoToolStripMenuItem,
+            this.listasDeReproducciónToolStripMenuItem,
+            this.reproductorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(474, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // descargarVídeoToolStripMenuItem
+            // 
+            this.descargarVídeoToolStripMenuItem.Name = "descargarVídeoToolStripMenuItem";
+            this.descargarVídeoToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.descargarVídeoToolStripMenuItem.Text = "Biblioteca";
+            this.descargarVídeoToolStripMenuItem.Click += new System.EventHandler(this.descargarVídeoToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 6;
+            // 
+            // ckbAudio
+            // 
+            this.ckbAudio.AutoSize = true;
+            this.ckbAudio.Location = new System.Drawing.Point(41, 243);
+            this.ckbAudio.Name = "ckbAudio";
+            this.ckbAudio.Size = new System.Drawing.Size(164, 17);
+            this.ckbAudio.TabIndex = 7;
+            this.ckbAudio.Text = "Eliminar después de convertir";
+            this.ckbAudio.UseVisualStyleBackColor = true;
+            // 
+            // listasDeReproducciónToolStripMenuItem
+            // 
+            this.listasDeReproducciónToolStripMenuItem.Name = "listasDeReproducciónToolStripMenuItem";
+            this.listasDeReproducciónToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
+            this.listasDeReproducciónToolStripMenuItem.Text = "Listas de Reproducción";
+            // 
+            // reproductorToolStripMenuItem
+            // 
+            this.reproductorToolStripMenuItem.Name = "reproductorToolStripMenuItem";
+            this.reproductorToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.reproductorToolStripMenuItem.Text = "Reproductor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(173, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(757, 520);
+            this.ClientSize = new System.Drawing.Size(474, 306);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ckbAudio);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtURL);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Convertir nuevo Vídeo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,9 +175,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtURL;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem descargarVídeoToolStripMenuItem;
+        private System.Windows.Forms.Timer tmrVideo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox ckbAudio;
+        private System.Windows.Forms.ToolStripMenuItem listasDeReproducciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reproductorToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Pro3Play/Pro3Play/Form1.cs
+++ Pro3Play/Pro3Play/Form1.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeExtractor;
+using NReco.VideoConverter;
+using VideoLibrary;
+using YoutubeExplode;
+using Tyrrrz.Extensions;
+using YoutubeExplode.Models.MediaStreams;
+using System.IO;
 
 namespace Pro3Play
 {
@@ -19,7 +26,75 @@ namespace Pro3Play
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Primer Commit a GitHub");
+            if (txtURL.Text == "")
+            {
+                MessageBox.Show("Por favor proporcione la URL del vídeo.");
+            }
+            else {
+                MainAsync();
+            }
+        }
+
+        private async Task MainAsync()
+        {
+            //nuevo cliente de Youtube
+            var client = new YoutubeClient();
+            //lee la dirección de youtube que le escribimos en el textbox
+            var videoId = NormalizeVideoId(txtURL.Text);
+            var video = await client.GetVideoAsync(videoId);
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(videoId);
+            // Busca la mejor resolución en la que está disponible el video
+            var streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
+            // Compone el nombre que tendrá el video en base a su título y extensión
+            var fileExtension = streamInfo.Container.GetFileExtension();
+            var fileName = $"{video.Title}.{fileExtension}";
+            //TODO: Reemplazar los caractéres ilegales del nombre
+            //fileName = RemoveIllegalFileNameChars(fileName);
+            //Activa el timer para que el proceso funcione de forma asincrona
+            tmrVideo.Enabled = true;
+            // mensajes indicando que el video se está descargando
+            label4.Text = "Descargando el video ... ";
+            //TODO: se pude usar una barra de progreso para ver el avance
+            //using (var progress = new ProgressBar())
+            //Empieza la descarga
+            await client.DownloadMediaStreamAsync(streamInfo, fileName);
+            //Ya descargado se inicia la conversión a MP3
+            var Convert = new NReco.VideoConverter.FFMpegConverter();
+            //Especificar la carpeta donde se van a guardar los archivos, recordar la \ del final
+            String SaveMP3File = @"C:\Users\Carlos Escobar\Source\Repos\programacion\Pro3Play\MP3\" + fileName.Replace(".mp4", ".mp3");
+            //Guarda el archivo convertido en la ubicación indicada
+            Convert.ConvertMedia(fileName, SaveMP3File, "mp3");
+            //Si el checkbox de solo audio está chequeado, borrar el mp4 despues de la conversión
+            if (ckbAudio.Checked)
+                File.Delete(fileName);
+            //Indicar que se terminó la conversion
+            MessageBox.Show("Vídeo convertido correctamente.");
+            label4.Text = "";
+            tmrVideo.Enabled = false;
+            //TODO: Cargar el MP3 al reproductor o a la lista de reproducción
+            //CargarMP3s();
+            //Se puede incluir un checkbox para indicar que de una vez se reproduzca el MP3
+            //if (ckbAutoPlay.Checked) 
+            //  ReproducirMP3(SaveMP3File);
+            return;
+        }
+
+        private static string NormalizeVideoId(string input)
+        {
+            string videoId = string.Empty;
+            return YoutubeClient.TryParseVideoId(input, out videoId)
+                ? videoId
+                : input;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void descargarVídeoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
